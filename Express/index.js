70a0fb3b604aa812e3dd5d99ba3121dd5b48e81e
Manzar <manzarabbasshah1@gmail.com>
2023-01-23 21:14:57 +0500

    Using express module

--- Express/index.js
+++ Express/index.js
@@ -1,34 +1,53 @@
 const fs =require('fs')
 const http =require('http')
+const express =require('express')
+const app = express();
 const port = 3000
 const hostname ='127.0.0.1';
-
 let index = fs.readFileSync('index.html')
 let about = fs.readFileSync('about.html')
 let project = fs.readFileSync('project.html')
 let contact = fs.readFileSync('contact.html')
-const server= http.createServer((req,res)=>{
-    url=req.url
-    res.statusCode=200
-    res.setHeader('Content-Type','text/html')
-    if(url == '/' || url == ''){
-        res.end(index)
-    }
-    else if(url == '/about'){
-        res.end(about)
-    }
-    else if(url == '/project'){
-        res.end(project)
-    }
-    else if(url == '/contact'){
-        res.end(contact)
-    }
-    else {
-    res.statusCode=404
 
-        res.end(`<h1 class='text-center'>404 Not Fount</h1>`)
-    }
+app.get("/", (req, res)=>{
+    res.send(index)
 })
-server.listen(port,hostname,()=>{
+
+app.get("/about", (req, res)=>{
+    res.send(about)
+})
+
+app.get("/project", (req, res)=>{
+    res.send(project)
+})
+
+app.get("/contact", (req, res)=>{
+    res.send(contact)
+})
+
+
+// const server= http.createServer((req,res)=>{
+//     url=req.url
+//     res.statusCode=200
+//     res.setHeader('Content-Type','text/html')
+//     if(url == '/' || url == ''){
+//         res.end(index)
+//     }
+//     else if(url == '/about'){
+//         res.end(about)
+//     }
+//     else if(url == '/project'){
+//         res.end(project)
+//     }
+//     else if(url == '/contact'){
+//         res.end(contact)
+//     }
+//     else {
+//     res.statusCode=404
+
+//         res.end(`<h1 class='text-center'>404 Not Fount</h1>`)
+//     }
+// })
+app.listen(port,hostname,()=>{
     console.log(`server is running on port http://${hostname}:${port}`)
 })
